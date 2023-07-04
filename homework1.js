1766f0a2cc6406618de8e1a436a1c1fdff84e070
vanswei <1049651706@qq.com>
2023-07-04 18:01:11 +0800

    2

--- homework1.js
+++ homework1.js
@@ -1,5 +1,6 @@
 const http = require('http')
 const server = http.createServer()
+//创建服务器
 server.on('request', function(req, res) { console.log('Someone visit our web server.') })
 server.listen(80, function() {
     console.log('server running at http://127.0.0.1:80')
