6ed65b1fa94d75a39bfaa963978ca9acd99f1891
vanswei <1049651706@qq.com>
2023-07-04 17:44:21 +0800

    1

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+    env: {
+        es6: true,
+        node: true,
+        mocha: true,
+    },
+    extends: 'airbnb-base',
+    root: true,
+    rules: {
+        'no-tabs': 0,
+        indent: [
+            'error',
+            'tab',
+        ],
+        'linebreak-style': [
+            'error',
+            'unix',
+        ],
+        quotes: [
+            'error',
+            'single',
+        ],
+        semi: [
+            'error',
+            'never',
+        ],
+    },
+}
