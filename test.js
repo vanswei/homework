0bf33233237349f5d72f46658344a4c7ae4ab708
vanswei <1049651706@qq.com>
2023-07-06 17:04:54 +0800

    修改了在服务器端确定的R值,并通过mocha测试

--- test.js
+++ test.js
@@ -6,10 +6,12 @@ const app = require('./homework3')
 describe('API测试', () => {
     describe('GET /start', () => {
         it('OK', (done) => {
+
             request(app)
                 .get('/start')
                 .expect(200)
                 .end((err, res) => {
+
                     should.not.exist(err)
                     res.text.should.equal('OK')
                     done();
@@ -19,7 +21,7 @@ describe('API测试', () => {
 
     describe('GET /:number', () => {
         it('当number<R时，smaller', (done) => {
-            const R = 50 // 假设R的值为50
+
 
             request(app)
                 .get('/25')
@@ -32,7 +34,7 @@ describe('API测试', () => {
         });
 
         it('当number>R时，bigger', (done) => {
-            const R = 50 // 假设R的值为50
+
 
             request(app)
                 .get('/75')
@@ -44,17 +46,20 @@ describe('API测试', () => {
                 });
         });
 
+
         it('当number=R时，equal', (done) => {
-            const R = 50 // 假设R的值为50
+
 
             request(app)
                 .get('/50')
                 .expect(200)
                 .end((err, res) => {
-                    should.not.exist(err)
+                    should.not.exist(err);
                     res.text.should.equal('equal')
                     done();
                 });
         });
+
+
     });
 });
