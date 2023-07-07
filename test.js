8e1fbbe993399f8ef536dda049feb3e9208f53e5
vanswei <1049651706@qq.com>
2023-07-07 12:01:15 +0800

    本次测试代码加入异步测试，并通过mocha测试

--- test.js
+++ test.js
@@ -1,23 +1,21 @@
 const should = require('should')
 const redis = require('redis')
 const request = require('supertest')
-
+const { promisify } = require('util')
 
 const app = require('./homework3')
 
 const client = redis.createClient()
-
+const getAsync = promisify(client.get).bind(client)
 
 describe('API测试', () => {
+
     describe('GET /start', () => {
         it('OK', (done) => {
-
             request(app)
                 .get('/start')
-
-            .expect(200)
+                .expect(200)
                 .end((err, res) => {
-
                     should.not.exist(err)
                     res.text.should.equal('OK')
                     done();
@@ -26,48 +24,27 @@ describe('API测试', () => {
     });
 
     describe('GET /:number', () => {
-        it('当number<R时，smaller', (done) => {
-
+        it('当number<R时，smaller', async() => {
             before(
                 client.set('R', 50)
-            ).then(done)
-            request(app)
-                .get('/25')
-                .expect(200)
-                .end((err, res) => {
-                    should.not.exist(err)
-                    res.text.should.equal('smaller')
-                    done();
-                })
+            )
+
+            const res = await request(app).get('/25').expect(200)
+            res.text.should.equal('smaller')
         });
 
-        it('当number>R时，bigger', (done) => {
-
-
-            request(app)
-                .get('/75')
-                .expect(200)
-                .end((err, res) => {
-                    should.not.exist(err);
-                    res.text.should.equal('bigger')
-                    done();
-                });
+        it('当number>R时，bigger', async() => {
+            const res = await request(app).get('/75').expect(200)
+            res.text.should.equal('bigger')
         });
 
-
-        it('当number=R时，equal', (done) => {
-
-
-            request(app)
-                .get('/50')
-                .expect(200)
-                .end((err, res) => {
-                    should.not.exist(err);
-                    res.text.should.equal('equal')
-                    done();
-                });
+        it('当number=R时，equal', async() => {
+            const res = await request(app).get('/50').expect(200)
+            res.text.should.equal('equal')
         });
+    });
 
-
+    after(async() => {
+        await client.quit()
     });
 });
