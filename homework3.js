03af60dd32881d74357d0746bdd33492a8739255
vanswei <138545072+vanswei@users.noreply.github.com>
2023-07-10 15:14:00 +0800

    Update homework3.js

--- homework3.js
+++ homework3.js
@@ -2,8 +2,8 @@ const express = require('express')
 const redis = require('redis')
 
 const app = express()
-const redisUrl = "redis://127.0.0.1:6379"
-const client = redis.createClient(redisUrl)
+
+const client = redis.createClient()
 
 
 //生成随机数R并保存
@@ -15,32 +15,31 @@ app.get('/start', (req, res) => {
 
 //比较number和R的值
 app.get('/:number', (req, res) => {
+    const number = parseInt(req.params.number)
     client.get('R', (err, reply) => {
-        //获取R的值
-        const R = parseInt(reply)
-        const number = parseInt(req.params.number)
-        if (number < R) {
-            res.send('smaller')
-        } else if (number > R) {
-            res.send('bigger')
-        } else if (number === R) {
-            const newR = Math.floor(Math.random() * 101)
-            client.set('R', newR)
-            res.send('equal')
+        if (err) {
+            // 处理错误
+            console.error(err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            const R = parseInt(reply);
+            //获取R的值
+            if (number < R) {
+                res.send('smaller')
+            } else if (number > R) {
+                res.send('bigger')
+            } else {
+                res.send('equal')
+                const newR = Math.floor(Math.random() * 101)
+                client.set('R', newR)
+
+            }
         }
     });
 });
 
 
 //启动服务器
-app.listen(3000, () => {
+module.exports = app.listen(3000, () => {
     console.log('Server started on port 3000')
-});
-//程序1
-app.listen(4000, () => {
-    console.log('Server1 started on port 4000')
-});
-//程序2
-app.listen(5000, () => {
-    console.log('Server2 started on port 5000')
 });
